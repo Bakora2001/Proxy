22389610000ab07873bc4a8404f90486643ad438
Maxwel Omwoyo <maxwellbakora@gmail.com>
2023-05-19 11:08:56 +0300

    Proxy is working

---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+import requests
+from lxml import html
+
+from flask import request
+from flask import Flask
+from flask import Response
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    usage = 'Pass a properly encoded url parameter e.g. /https/www.google.com'
+    return usage
+
+
+@app.route('/https/<url>')
+def root(url):
+    url = 'https://' + url
+    r = requests.get(url)
+    rr = Response(response=r.content, status=r.status_code)
+    rr.headers["Content-Type"] = r.headers['Content-Type']
+    return rr
+
+
+@app.route('/g/<keyword>')
+def gkeyword(keyword):
+    url = 'https://www.google.com/search'
+    payload = {'q': keyword, 'num': 1, 'start': 1,
+               'sourceid': 'chrome', 'ie': 'UTF-8', 'cr': 'cr=countryUS'}
+    r = requests.get(url, params=payload)
+    rr = Response(response=r.content, status=r.status_code)
+    rr.headers["Content-Type"] = r.headers['Content-Type']
+    return rr
+
+
+@app.route('/r/<subreddit>/subscribers')
+def gsubreddit(subreddit):
+    url = 'https://old.reddit.com/r/' + subreddit
+    xpath = "//span[@class='subscribers']/span[@class='number']/text()"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    tree = html.fromstring(r.content)
+    subscribers = tree.xpath(xpath)
+    rr = Response(response=subscribers, status=r.status_code)
+    rr.headers["Content-Type"] = r.headers['Content-Type']
+    return rr
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
